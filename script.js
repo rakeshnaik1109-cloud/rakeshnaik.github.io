6870dbd882994efe3f8269b57bd760b050492ec8
rakeshnaik1109-cloud <rakeshnaik1109@gmail.com>
2026-05-25 22:11:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+<script>
+const modal=document.getElementById("imgModal");
+const img=document.getElementById("researchImg");
+const modalImg=document.getElementById("expandedImg");
+const closeBtn=document.querySelector(".close");
+
+img.onclick=function(){
+    modal.style.display="block";
+    modalImg.src=this.src;
+}
+
+closeBtn.onclick=function(){
+    modal.style.display="none";
+}
+
+modal.onclick=function(e){
+    if(e.target===modal){
+        modal.style.display="none";
+    }
+}
+</script>
+
+
+
 /* =====================================================
    PUBLICATION AUTO-FORMAT (With Author Highlighting)
 ===================================================== */
